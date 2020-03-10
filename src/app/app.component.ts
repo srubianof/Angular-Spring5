4c1dec976cd40284b92fc9eb57f2ad83531b9fa7
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2020-03-09 22:15:54 -0500

    editando

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cliente-app';
+  title = 'Bienvenido a Angular';
+  curso: string = 'Curso Spring 5 con Angular 7';
+  profesor: string = 'Andres Guzman ';
 }
