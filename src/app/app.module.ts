1cc8c9d61e3ddd225681bd9cda76f1f1a76a1fd4
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2020-03-17 22:08:16 -0500

    Creando componentes extra, y agregandolos a la pagina principal
    Se crea el footer se agrega y se cambian colores del navbar y footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponet } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponet
+    HeaderComponet,
+    FooterComponent
   ],
   imports: [
     BrowserModule
