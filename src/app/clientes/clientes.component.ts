93091c32d982ca73cf122213e3fd2995c268e5db
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2020-03-17 22:53:31 -0500

    Separar info tabla
    Se separa la info y se crea un json para simular que esta viene de un API-rest o una DB.

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,22 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
+import { CLIENTES } from './clientes.json';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
 })
 export class ClientesComponent implements OnInit {
 
-  clientes: Cliente[] = [
-    { id: 1, nombre: 'a', apellido: 'a', email: 'a@a.com', createAt: '2020-03-17' },
-    { id: 2, nombre: 'b', apellido: 'b', email: 'b@b.com', createAt: '2020-03-17' },
-    { id: 3, nombre: 'c', apellido: 'c', email: 'c@c.com', createAt: '2020-03-17' },
-    { id: 4, nombre: 'd', apellido: 'd', email: 'd@d.com', createAt: '2020-03-17' },
-    { id: 5, nombre: 'e', apellido: 'e', email: 'e@e.com', createAt: '2020-03-17' },
-    { id: 6, nombre: 'f', apellido: 'f', email: 'f@f.com', createAt: '2020-03-17' }
-  ]
+  clientes: Cliente[];
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.clientes=CLIENTES;
   }
 
 }
---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+export const CLIENTES: Cliente[] = [
+  { id: 1, nombre: 'a', apellido: 'a', email: 'a@a.com', createAt: '2020-03-17' },
+  { id: 2, nombre: 'b', apellido: 'b', email: 'b@b.com', createAt: '2020-03-17' },
+  { id: 3, nombre: 'c', apellido: 'c', email: 'c@c.com', createAt: '2020-03-17' },
+  { id: 4, nombre: 'd', apellido: 'd', email: 'd@d.com', createAt: '2020-03-17' },
+  { id: 5, nombre: 'e', apellido: 'e', email: 'e@e.com', createAt: '2020-03-17' },
+  { id: 6, nombre: 'f', apellido: 'f', email: 'f@f.com', createAt: '2020-03-17' }
+];
