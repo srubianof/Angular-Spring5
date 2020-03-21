a08d171df64231caf29174c0ed8bd4d180242c59
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2020-03-21 14:18:40 -0500

    Seccion 37 creacion clases Repository y Service
    Se crean clases que me permiten la creacion de las tablas en la DB ademas se crean los dao (como accedo a los datos de la DB),  las entidades (como convierto lo SQL a Java) y los Services (consultas)

---
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -0,0 +1,62 @@
+package com.bolsadeideas.springboot.backend.apirest.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+
+@Entity
+@Table(name="clientes")
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String nombre;
+    private String apellido;
+    private String email;
+    @Column(name = "create_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
