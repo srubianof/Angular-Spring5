5b038e36936d501e63156d9e9e0c2bba03f16a46
Santiago Rubiano <43104565+srubianof@users.noreply.github.com>
2020-03-25 18:15:04 -0500

    Leccion 42 y 43 creando mas metodos (update, delete, etc)

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRESTController.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRESTController.java
@@ -3,11 +3,10 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
 import com.bolsadeideas.springboot.backend.apirest.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @CrossOrigin(origins = {"http://localhost:4200"})
@@ -16,13 +15,56 @@ import java.util.List;
 @RequestMapping("/api")
 public class ClienteRESTController {
 
-    //Inyecto un componente Spring buscando una implementacion contreta (ClienteServiceImlp)
-    @Autowired
-    private IClienteService clienteService;
+  //Inyecto un componente Spring buscando una implementacion contreta (ClienteServiceImlp)
+  @Autowired
+  private IClienteService clienteService;
 
-    //Mapeo para que el metodo de tipo GET se pueda utilizar
-    @GetMapping("/clientes")
-    public List<Cliente> index() {
-        return clienteService.findAll();
-    }
+  //Mapeo para que el metodo de tipo GET se pueda utilizar
+  @GetMapping("/clientes")
+  public List<Cliente> index() {
+    return clienteService.findAll();
+  }
+
+
+  //Mapeo para que el metodo de tipo GET me retorne clientes por id
+  @GetMapping("/clientes/{id}")
+  //  Retorno el estado de la creacion (por defecto es OK)
+//  @ResponseStatus(HttpStatus.OK)
+  public Cliente show(@PathVariable Long id) {
+    return clienteService.findById(id);
+  }
+
+
+  //Mapeo para que el metodo de tipo POST me cree un objeto cliente
+  //nos envian el cliente en formato json (por eso requestbody) y lo transformo al objeto cliente
+  @PostMapping("/clientes")
+//  Retorno el estado de la creacion
+  @ResponseStatus(HttpStatus.CREATED)
+  public Cliente create(@RequestBody Cliente cliente) {
+//    ESTA MANERA DE CREAR FECHAS QUEDA EN LA ENTIDAD CLIENTE MEJOR PUESTA
+//    cliente.setCreateAt(new Date());
+    return clienteService.save(cliente);
+  }
+
+
+  // tener metodo update y su verbo es el PUT
+  @PutMapping("/clientes/{id}")
+  @ResponseStatus(HttpStatus.CREATED)
+  public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
+    Cliente clienteAcutal = clienteService.findById(id);
+//    modificar cliente actual (viene de DB)
+    clienteAcutal.setApellido((cliente.getApellido()));
+    clienteAcutal.setNombre((cliente.getNombre()));
+    clienteAcutal.setEmail((cliente.getEmail()));
+
+    return clienteService.save(clienteAcutal);
+
+  }
+
+
+  @DeleteMapping("/clientes/{id}")
+  @ResponseStatus(HttpStatus.NO_CONTENT)
+  public void delete(@PathVariable Long id) {
+    clienteService.delete(id);
+  }
 }
--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
+++ spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/entity/Cliente.java
@@ -9,56 +9,62 @@ import java.util.Date;
 @Table(name = "clientes")
 public class Cliente implements Serializable {
 
-    //id de la tabla ademas el identity me permite que se incremente automaticamente
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+  //id de la tabla ademas el identity me permite que se incremente automaticamente
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
 
-    private String nombre;
-    private String apellido;
-    private String email;
-    @Column(name = "create_at")
-    @Temporal(TemporalType.DATE)
-    private Date createAt;
+  private String nombre;
+  private String apellido;
+  private String email;
+  @Column(name = "create_at")
+  @Temporal(TemporalType.DATE)
+  private Date createAt;
 
-    public Date getCreateAt() {
-        return createAt;
-    }
+  //antes de que se haga un save incluya la fecha
+  @PrePersist
+  public void prePersist() {
+    createAt = new Date();
+  }
 
-    public void setCreateAt(Date createAt) {
-        this.createAt = createAt;
-    }
+  public Date getCreateAt() {
+    return createAt;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public void setCreateAt(Date createAt) {
+    this.createAt = createAt;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public String getApellido() {
-        return apellido;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
+  public String getApellido() {
+    return apellido;
+  }
 
-    public String getNombre() {
-        return nombre;
-    }
+  public void setApellido(String apellido) {
+    this.apellido = apellido;
+  }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
+  public String getNombre() {
+    return nombre;
+  }
+
+  public void setNombre(String nombre) {
+    this.nombre = nombre;
+  }
 
 
-    public long getId() {
-        return id;
-    }
+  public long getId() {
+    return id;
+  }
 
-    public void setId(long id) {
-        this.id = id;
-    }
+  public void setId(long id) {
+    this.id = id;
+  }
 }
